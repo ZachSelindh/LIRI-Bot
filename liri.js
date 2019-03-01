0327535821ebe330675a57493a437db41d10020b
Zach Selindh <zachselindh@gmail.com>
2019-02-28 18:34:42 -0600

    Added switch and empty functions.

--- liri.js
+++ liri.js
@@ -1,16 +1,19 @@
 // Javascript file for LIRI-bot Node assignment
 
-require("dotenv").config();
-const spotify =  require("node-spotify-api");
+const dotenv = require("dotenv").config();
+const Spotify = require("node-spotify-api");
 const keys = require("./keys.js");
 const axios = require("axios");
 const fs = require("fs");
 const moment = require("moment");
 
+let spotify = new Spotify(keys.spotify);
+
 let input = process.argv;
 let command = process.argv[2];
 let searchInput = "";
 
+/* For Loop to take in the console input and convert it to a new variable */
 for (i = 3; i < input.length; i++) {
     if (i > 3 && i < input.length) {
         searchInput += searchInput + "+" + input[i];
@@ -20,8 +23,38 @@ for (i = 3; i < input.length; i++) {
     console.log(searchInput)
 }
 
-if (searchInput !== "") {
-    let userInput = command + ":"
+function doSpotify(searchInput) {
+
+}
+
+function doBandsInTown(searchInput) {
+
+}
+
+function doOMDB(searchInput) {
+
+}
+
+function doWhatItSays() {
+
+}
+
+/* Switch statment that uses the command variable to run the correct function. */
+switch(command) {
+    case "spotify-this-song":
+        doSpotify();
+        break;
+    case "concert-this":
+        doBandsInTown();
+        break;
+    case "movie-this":
+        doOMDB();
+        break;
+    case "do-what-it-says":
+        doWhatItSays;
+        break;
+    default:
+        break;
 }
 
 /* Link for BIT API
